153f50fcc1c129b76e6450835d73b4b599e9d09b
aleffqs <aleffqs@hotmail.com>
2021-09-09 23:31:51 -0300

    Exercicio animais

---
+++ Java/src/exemplos/AreaTriangulo.java
@@ -0,0 +1,55 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class AreaTriangulo {
+
+	public static void main(String[] args) {
+	    /*Descubra a área do que o usuário passar
+	     * Trapézio: A = ((B + b)/2) * h
+	     * B = Base maior, b = base menor, h = altura
+	     * 
+	     * Triângulo: A = (B*h)/2
+	     * B = Base, h = altura
+	     */
+
+
+	    System.out.println("Bem-vindo ao calculador de área!");
+	    System.out.println("Informe qual objeto quer calcular: ");
+
+	    Scanner input = new Scanner(System.in);
+
+	    boolean trapézio = true, triângulo = true;
+
+	    if(trapézio){
+	        Double B,b,h,A;
+	        
+	        System.out.println("Informe o valor de B = base maior: ");
+	        B = input.nextDouble();
+	        System.out.println("Informe o valor de b = base menor: ");
+	        b = input.nextDouble();
+	        System.out.println("Informe o valor de h = altura");
+	        h = input.nextDouble();
+	        //calculando
+	        A = ((B + b)/2)*h;
+	        System.out.println("O valor dado é B: "+B+" b: "+b+" h: "+h);
+	        System.out.println("A área do trapézio é: "+A);
+	        input.close();
+	    }
+	    if(triângulo){
+	        Double B,h,A;
+	        
+	        System.out.println("Informe o valor de B = base: ");
+	        B = input.nextDouble();
+	        System.out.println("Informe o valor de h = altura: ");
+	        h = input.nextDouble();
+	        //calculando
+	        A = (B*h)/2;
+	        System.out.println("O valor dado é B: "+B+" h: "+h);
+	        System.out.println("A área do triângulo é: "+A);
+	        input.close();
+	    }   
+
+
+	}
+}
---
+++ Java/src/module-info.java
@@ -0,0 +1,8 @@
+module BANCO {
+	exports Lista1;
+	exports aula2;
+	exports aula4;
+	exports turma33java;
+	exports lista3;
+	exports exemplos;
+}
